a0955b40d9a985e9abd0727c337403d3e397039d
sreyleng.chheang <sreyleng.chheang222@gmail.com>
2020-07-10 13:40:41 +0700

    #4 complete menu

---
+++ app/Controllers/TestMenu.php
@@ -0,0 +1,12 @@
+<?php namespace App\Controllers;
+
+class TestMenu extends BaseController
+{
+	public function index()
+	{
+		return view('index');
+	}
+
+	//--------------------------------------------------------------------
+
+}
