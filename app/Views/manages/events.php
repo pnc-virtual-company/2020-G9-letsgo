fba93c2684c49ed39d7b0f4c1b4fd3acdb06d9bf
puthy ouk <puthy.ouk168@gmail.com>
2020-07-11 10:35:53 +0700

    #15 addModel

--- app/Views/manages/events.php
+++ app/Views/manages/events.php
@@ -32,7 +32,9 @@
         <td>Soccer Competition</td>
         <td>Sport</td>
         <td>25/05/2005</td>
-        <td class="deleteEvent"><i class="large material-icons ">delete</i></td>
+        <td class="deleteEvent"><a href="" data-toggle="modal" data-target="#removeEvent" title="Delete event!" 
+         data-placement="right"><i class="material-icons text-danger">delete</i></a>
+        </td>
       </tr>
       <tr class="info">
         <td>Ronan</td>
@@ -40,7 +42,9 @@
         <td>Piano</td>
         <td>Music</td>
         <td>25/05/2005</td>
-        <td class="deleteEvent" ><i class="large material-icons" >delete</i></td>
+        <td class="deleteEvent"><a href="" data-toggle="modal" data-target="#removeEvent" title="Delete event!" 
+         data-placement="right"><i class="material-icons text-danger">delete</i></a>
+        </td>
       </tr>
       <tr class="info">
         <td>Seiha</td>
@@ -48,12 +52,48 @@
         <td>Poker</td>
         <td>Games</td>
         <td>25/05/2005</td>
-        <td class="deleteEvent" ><i class="large material-icons ">delete</i></td>
+        <td class="deleteEvent"><a href="" data-toggle="modal" data-target="#removeEvent" title="Delete event!" 
+         data-placement="right"><i class="material-icons text-danger">delete</i></a>
+        </td>
       </tr>
  
     </tbody>
   </table>
     </div>
  </div>
+
+
+
+
+<!-- The Modal Remove Event-->
+<div class="modal mt-5" id="removeEvent">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            
+                <!-- Modal Header -->
+                <div class="modal-header">
+                    <h4 class="modal-title">Remove itme ?</h4>
+                </div>
+                
+                <!-- Modal body -->
+                <div class="modal-body mt-2">
+                    Are you sure you want to remove selected item ?
+                </div>
+                
+                <!-- Modal footer -->
+                <div class="modal-footer">
+                    <a href="" class="text-uppercase text-dark">DON'T REMOVE</a>
+                    <a href="" class="text-uppercase text-warning">REMOVE</a>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+    <!-- end Modal Remove Category-->
+
+
+
+
 </div>
 <?= $this->endSection() ?>
+
