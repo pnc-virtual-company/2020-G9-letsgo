9e38b8d309df5c371f21d0e475d920a1dbbbb84c
ronanpnc <ronan.ogor@passerellesnumeriques.org>
2020-07-28 20:18:26 +0700

    #0 Update the base URL on app to test production

--- app/Config/App.php
+++ app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'http://localhost:8080/';
+	public $baseURL = '3.85.33.128/2020-G9-letsgo/public';
 
 	/*
 	|--------------------------------------------------------------------------
