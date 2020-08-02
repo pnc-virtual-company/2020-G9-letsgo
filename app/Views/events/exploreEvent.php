0e5b7a2a041646a547d4613beecd27b905541716
puthy ouk <puthy.ouk168@gmail.com>
2020-08-02 16:18:36 +0700

    #43 Change button on card in exploreEvent

--- app/Views/events/exploreEvent.php
+++ app/Views/events/exploreEvent.php
@@ -42,34 +42,29 @@
 </div>
 
 
+
 <div class="container mt-5">
-  <!-- Nav tabs -->
-  <ul class="nav nav-tabs">
+<ul class="nav nav-tabs">
     <li class="nav-item event ">
       <a class="nav-link active" href="#menu1">CARDS</a>
     </li>
     <li class="nav-item calendar">
       <a class="nav-link" href="#menu2">CALENDAR</a>
     </li>
-  </ul>
-
-  <!-- Tab panes -->
-
-  <div class="tab-content">
-  
+</ul>
+<br>
+<div class="tab-content">
     <div id="menu1" class="container tab-pane active"><br>
     <?php foreach($eventData as $values) :?>
         <?php               
             $date = date('Y-m-d');          
         ?>
         <?php  if ($values['end_date'] >= $date): ?>
-        <br>  
        
-        <p id="myDate"> <?php $date = new DateTime($values['start_date']);?>
-            <?= date_format($date, 'l/d/F/Y'); ?>
-        </p>
+        <?php $date = new DateTime($values['start_date']);?>
+        <?= date_format($date, 'l/d/F/Y'); ?>
         
-        <div class="card mt-4 card-explore" data-toggle="modal" data-target="#exampleModalCenter">
+        <div class="card mt-4 card-explore" id="event"  >
             <div class="card-body">
                 <div class="row">
                     <div class="col-sm-3">
@@ -78,195 +73,86 @@
                         <?php $date = new DateTime($values['start_time']);?>
                         <?= date_format($date, 'g:i A'); ?>
                     </div>
-                    <div class="col-sm-4">
+                    <div class="col-sm-4"  data-toggle="modal"  data-target="#eventDetail">
                         <p><?= $values['name']; ?></p>
                         <h2><?= $values['title']; ?></h2>
                         <span>4 member going</span>
                     </div>
-                    <div class="col-sm-3">
+                    <div class="col-sm-3" data-toggle="modal"  data-target="#eventDetail">
                         <br>
-                        <div class="text-center">
+                        <div class="text-center" data-toggle="modal"  data-target="#eventDetail">
                             <img src="images/game.jpeg" class="rounded img-explore" alt="Cinque Terre"> 
                         </div>
                     </div>
-                    <div class="col-sm-2">
+                    <div class="col-sm-2" data-toggle="modal">
                         <br>
-                        <br>
-                        <span class="badge badge-light" ><i class="material-icons">highlight_off</i>Quit</span>
+                        <button class="btn btn-sm btn btn-danger mt-4 "  id="quit1" >
+                            <i class="fa fa-times-circle"></i>
+                            <b>Quit</b>
+                        </button>
+
+                        <button class="btn btn-sm btn btn-success mt-4 "  id="quitjoin" >
+                            <i class="fa fa-times-circle"></i>
+                            <b>Join</b>
+                        </button>
                     </div>
+
                 </div>
-            </div>
-        </div>
-       <?php endif; ?>
-    <?php endforeach; ?>
-        
-        <div class="container mt-5">
-        <div class="modal fade " id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered " role="document">
-                <div class="modal-dialog modal-lg">
-                    <div class="modal-content">
-                        <div class="modal-body modal-explore">
-                            <div class="container">
-                                <div class="row">
-                                    <div class="col-sm-6">
-                                        <img src="images/game.jpeg" class="rounded img-explore" alt="Cinque Terre">
-                                    </div>
+            </div>   
+        </div>         
+    <?php endif; ?>
+<?php endforeach; ?>
 
-                                    <div class="col-sm-6">
-                                        <span>category</span>
-                                        <h3>title event</h3>
-                                        <span><i class="material-icons">place</i>phnom peng</span><br>
-                                        <span><i class="material-icons">people_outline</i>74 members</span><br>
-                                        <span><i class="material-icons">perm_identity</i>organized by: user</span><br>
-                                        <span><i class="material-icons">query_builder</i>moday, july 6 4pm to 5pm</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <hr>
-                            <span>Are you intereted in finding how to live trul?</span><br>
-                            <span>Join us and we will show you the path step by step</span><br>
-                            <span>Let's do it together!</span><br>
-                            <span>click for More Info : Our Websit <a href="#">https://www.event.com</a></span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    </div>
-    <div id="menu2" class="container tab-pane fade"><br>
-    <div class="container py-5">
-    <!-- Calendar -->
-    <div class="calendar shadow bg-white p-5">
-    <div class="d-flex align-items-center"><i class="fa fa-calendar fa-3x mr-3"></i>
-            </div>
-            <ol class="day-names list-unstyled">
-                <li class="font-weight-bold text-uppercase">Sun</li>
-                <li class="font-weight-bold text-uppercase">Mon</li>
-                <li class="font-weight-bold text-uppercase">Tue</li>
-                <li class="font-weight-bold text-uppercase">Wed</li>
-                <li class="font-weight-bold text-uppercase">Thu</li>
-                <li class="font-weight-bold text-uppercase">Fri</li>
-                <li class="font-weight-bold text-uppercase">Sat</li>
-            </ol>
-
-            <ol class="days list-unstyled">
-                <li>
-                <div class="date">1</div>
-                <div class="event bg-success">Event with Long Name</div>
-                </li>
-                <li>
-                <div class="date">2</div>
-                </li>
-                <li>
-                <div class="date">3</div>
-                </li>
-                <li>
-                <div class="date">4</div>
-                </li>
-                <li>
-                <div class="date">5</div>
-                </li>
-                <li>
-                <div class="date">6</div>
-                </li>
-                <li>
-                <div class="date">7</div>
-                </li>
-                <li>
-                <div class="date">8</div>
-                </li>
-                <li>
-                <div class="date">9</div>
-                </li>
-                <li>
-                <div class="date">10</div>
-                </li>
-                <li>
-                <div class="date">11</div>
-                </li>
-                <li>
-                <div class="date">12</div>
-                </li>
-                <li>
-                <div class="date">13</div>
-                <div class="event all-day begin span-2 bg-warning">Event Name</div>
-                </li>
-                <li>
-                <div class="date">14</div>
-                </li>
-                <li>
-                <div class="date">15</div>
-                <div class="event all-day end bg-success">Event Name</div>
-                </li>
-                <li>
-                <div class="date">16</div>
-                </li>
-                <li>
-                <div class="date">17</div>
-                </li>
-                <li>
-                <div class="date">18</div>
-                </li>
-                <li>
-                <div class="date">19</div>
-                </li>
-                <li>
-                <div class="date">20</div>
-                </li>
-                <li>
-                <div class="date">21</div>
-                <div class="event bg-primary">Event Name</div>
-                <div class="event bg-success">Event Name</div>
-                </li>
-                <li>
-                <div class="date">22</div>
-                <div class="event bg-info">Event with Longer Name</div>
-                </li>
-                <li>
-                <div class="date">23</div>
-                </li>
-                <li>
-                <div class="date">24</div>
-                </li>
-                <li>
-                <div class="date">25</div>
-                </li>
-                <li>
-                <div class="date">26</div>
-                </li>
-                <li>
-                <div class="date">27</div>
-                </li>
-                <li>
-                <div class="date">28</div>
-                </li>
-                <li>
-                <div class="date">29</div>
-                </li>
-                <li>
-                <div class="date">30</div>
-                </li>
-                <li>
-                <div class="date">31</div>
-                </li>
-                <li class="outside">
-                <div class="date">1</div>
-                </li>
-                <li class="outside">
-                <div class="date">2</div>
-                </li>
-                <li class="outside">
-                <div class="date">3</div>
-                </li>
-                <li class="outside">
-                <div class="date">4</div>
-                </li>
-            </ol>
-            </div>
-            </div>
-        </div>
-  </div>
+<!-- The Modal -->
+<div class="modal fade" id="eventDetail">
+    <div class="modal-dialog">
+    <div class="modal-content">
+<!-- Modal Header -->
+<div class="modal-header">
+    <h4 class="modal-title text-warning">Event Detail</h4>
 </div>
+<div class="container-fluid">
+<div class="row">
+<div class="col-4">
+<!-- <div class="card" style="width: 125px; margin-top: 15px; height: 125px"> -->
+    <img class="rounded-circle mt-5" style="width: 140px; height: 125px" src="">
+<!-- </div> -->
+</div>
+<div class="col-8">
+    <p class="category text-primary"></p>
+    <p class="category text-primary"><?= $values['name']; ?></p>
+    <h4 class="title"><strong><?= $values['title']; ?></strong></h4>
+<div class="row">
+    <i class="material-icons">location_on</i>
+    <p> </p>
+</div>
+
+<div class="row">
+    <i class="material-icons">account_circle</i>
+    <p>16 members</p>
+</div>
+
+<div class="row">
+    <i class="material-icons">account_circle</i>
+<p></p>
+<!-- <p>Organized by: Seiha</p> -->
+</div>
+
+<div class="row">
+    <i class="material-icons">alarm</i>
+    <p><?php $date = new DateTime($values['start_time']);?>
+       <?= date_format($date, 'g:i A'); ?></p>
+</div>
+    <a href="" id="hide" class="btn btn-success float-right"><i class="fa fa-check-circle" style="color:white"></i>Join</a>
+</div>
+</div>
+<hr>
+
+</div>
+    <div class="container">
+        <textarea cols="55" rows="5">  </textarea>
+    </div>
+</div>
+
+
 
