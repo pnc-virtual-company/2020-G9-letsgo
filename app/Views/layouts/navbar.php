cd442317cd90fe4417e0c8d98dca2d462089a574
sreyneang040 <sreyneang.nov0909@gmail.com>
2020-08-08 10:09:51 +0700

    #62 Updated menu highlights and spelling of explore event

--- app/Views/layouts/navbar.php
+++ app/Views/layouts/navbar.php
@@ -2,20 +2,20 @@
 <?= $this->section('content') ?>
 <!-- user login show menu -->
 <?php if(session()->get('isLoggedIn')) :?>
-<nav class="navbar navbar-expand-sm navbar-light bg-warning">
+<nav class="navbar navbar-expand-sm navbar-light bg-warning ">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse " id="navbarSupportedContent">
   <ul class="nav navbar-nav ml-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="<?= base_url('explore')?>">Explor event<span class="sr-only">(current)</span></a>
-      </li>
       <li class="nav-item">
-        <a class="nav-link" href="<?= base_url('yourEvents')?>">Your event</a>
+        <a class="nav-link" href="<?= base_url('explore')?>">Explore event</a>
       </li>
-      <li class="nav-item dropdown">
+      <li class="nav-item active">
+        <a class="nav-link" href="<?= base_url('yourEvents')?>">Your event<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item dropdown ">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Manage
         </a>
@@ -24,7 +24,7 @@
           <a class="dropdown-item" href="<?= base_url('category')?>">Category</a>
       </div>
       </li>
-      <li class="nav-item dropdown">
+      <li class="nav-item dropdown ">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <!-- Get first name display in menu -->
             <?= $getUser['first_name'] ?>
